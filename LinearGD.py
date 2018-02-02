4018d69de406b27bb713c19d71ad6cfe46116350
Sharma <vishesh.a.sharma@accenture.com>
2018-02-02 22:44:53 +0530

    Initial Commit

---
+++ LinearGD.py
@@ -0,0 +1,16 @@
+from numpy import *
+
+def run():
+	points = genfromtxt('dataGD.csv',delimiter=',')
+	#hyperparamters
+	learning_rate = 0.0001
+	#y=mx+b
+	intial_m = 0
+	intial_b = 0
+	num_interations = 1000
+	
+
+
+
+if __name__ == "__main__":
+	run()
---
+++ README.md
@@ -0,0 +1,1 @@
+#Collection of Pandas tutorial code
